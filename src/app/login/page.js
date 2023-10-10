dd5664f8652bfe5e11e924963455af6ab46ca072
zulmee <leejmtv22@gmail.com>
2023-10-10 14:02:25 +0900

    정민커밋

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,65 +1,30 @@
-"use client"
-import Link from 'next/link';
-import React from 'react';
+export default function RootLayout({ children }) {
 
-import { useRef } from 'react';
+	return (
+		<form action="/pages/api/login/in/route" method='POST'>
+			<div className="wrap">
+				<div className="login1">
+					<h1>
+						<img src="images/logo.jpg" alt="milan information system" />
+					</h1>
+					<div className="loginBox">
 
+						<input name="loginID" placeholder="아이디" />
+						<input name="password" placeholder="비밀번호" />
+						<div>
+							<input type="checkbox" className="save" name="key" />
+							<label for="saveId">아이디 저장</label>
+							<input type="checkbox" className="save" name="pass" />
+							<label for="savePw">비밀번호 저장</label>
+						</div>
+						<a href="">회원가입</a>
+						{/* <button type="submit" onClick={() => { fetch('/api/login', { method: 'POST', body: JSON.stringify(userdata)}),router.push('/') }} className="btnLogin" >로그인</button> */}
+						<button type='submit'>버튼</button>
 
-export default async function RootLayout({ children }) {
-	const usernameRef = useRef(null);
-	const emailRef = useRef(null);
-	const passwordRef = useRef(null);
-  
-	const handleSubmit = async (e) => {
-	  e.preventDefault();
-  
-	  const formData = {
-		username: usernameRef.current.value,
-		//email: emailRef.current.value,
-		password: passwordRef.current.value
-	  };
-  
-	  try {
-		const response = await fetch('http://localhost:8080//api/member', {
-		  method: 'POST',
-		  headers: {
-			'Content-Type': 'application/json'
-		  },
-		  body: JSON.stringify(formData)
-		});
-  
-		if (response.ok) {
-		  const data = await response.json();
-		  console.log('Registration successful:', data);
-		} else {
-		  console.error('Registration failed:', response.statusText);
-		}
-	  } catch (error) {
-		console.error('An error occurred during registration:', error);
-	  }
-	};
+					</div>
+				</div>
+			</div>
+		</form>
 
-    return (
-      <form onSubmit={handleSubmit}>
-        <div className="wrap">
-         <div className="login1">
-
-            <h1>
-               <img src="images/logo.jpg" alt="logo"/>
-            </h1>
-            <div className="loginBox">
-               <input type="text" ref={usernameRef} placeholder="아이디"/>
-               <input type="password" ref={passwordRef} placeholder="비밀번호"/>
-               <div>
-                  <input type="checkbox" className="save" name="key" /> 
-                  <label for="saveId">아이디 저장</label> 
-
-               </div>
-               <Link href="/signin">회원가입</Link>
-               <Link href="/"><button type="submit" className="btnLogin">로그인</button></Link>
-            </div>
-         </div>
-      </div>
-      </form>
-        )
-    }
+	)
+}
