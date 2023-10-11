52b20ff18679967e672f242e18a5c4c6ff2a56a0
zulmee <leejmtv22@gmail.com>
2023-10-11 10:56:42 +0900

    로그인 api 수정 v2

--- src/app/api/login/in/route.js
+++ src/app/api/login/in/route.js
@@ -3,20 +3,16 @@ import { NextResponse } from "next/server";
 export async function POST(req) {
   // if (req.method == 'POST') {
     try {
-      const body = await req.json()
       console.log("post요청옴")
-      console.log(body)
+      console.log(await req.json())
+      
       //fetch("백 url").then("로그인응답내용")
-
+      
       //임시로 데이터요청오면 메인으로 감
       res.redirect(302, "/")
     } catch (error) {
-      //에러시 실행할코드
+      //에러시 실행할코드~~
     }
     return NextResponse.json({ message: "Hello from Next.js" }, { status: 200 });
   // }
 }
-
-
-//   const data = await res.json()
-//   return Response.json(data)
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,28 +2,32 @@
 import { useRouter } from "next/navigation";
 import { headers } from "../../../next.config";
 
+//폼 버튼 레이아웃 수정필요!!
 
 export default function RootLayout({ children }) {
-	const router = useRouter();
+	const router = useRouter(); 
 	return (
 		<form onSubmit={(e) => {
 			e.preventDefault();
+			//폼 name값 변수저장
 			const loginID = e.target.loginID.value;
 			const password = e.target.password.value;
+			//fetch로 보낼 옵션들
 			const options = {
 				method: 'POST',
 				headers: {
 					//json타입으로 전송
 					'Content-Type': 'application/json'
 				},
+				//전송할 데이터 json으로 변환해서 body에 넣어줌
 				body: JSON.stringify({ loginID, password })
 			}
-			fetch('api/login/in', options)
-				.then(res => res.json())
+			fetch('api/login/in', options) //경로 및 옵션들
+				.then(res => res.json()) //res를 json으로 전송
 				.then(result => {
 					console.log("응답옴")
 					console.log(result);
-					//로그인성공하면 메인으로
+					//로그인성공하면 메인으로(임시)
 					router.push('/')
 				})
 		}}>
@@ -46,28 +50,6 @@ export default function RootLayout({ children }) {
 					</div>
 				</div>
 			</div>
-
 		</form>
-		// <form action="/api/login/in" method="POST">
-		// 	<div className="wrap">
-		// 		<div className="login1">
-		// 			<h1>
-		// 				<img src="images/logo.jpg" alt="milan information system" />
-		// 			</h1>
-		// 			<div className="loginBox">
-		// 				<input name="loginID" placeholder="아이디" />
-		// 				<input name="password" placeholder="비밀번호" />
-		// 				<div>
-		// 					<input type="checkbox" className="save" name="key" />
-		// 					<label for="saveId">아이디 저장</label>
-		// 					<input type="checkbox" className="save" name="pass" />
-		// 					<label for="savePw">비밀번호 저장</label>
-		// 				</div>
-		// 				<a href="">회원가입</a>
-		// 				<SubmitButton />
-		// 			</div>
-		// 		</div>
-		// 	</div>
-		// </form>
 	)
 }
