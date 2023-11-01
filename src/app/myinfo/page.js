b3cd6d737cce369700db37095c867c8b3029160e
gonggongtae <dnjsxo0306@gmail.com>
2023-11-01 18:30:45 +0900

    구독 페이지 체크박스 기능 수정 중

--- src/app/myinfo/page.js
+++ src/app/myinfo/page.js
@@ -1,45 +1,44 @@
-'use client'
-import Link from 'next/link';
+"use client";
+import { SUBRESOURCE_INTEGRITY_MANIFEST } from "next/dist/shared/lib/constants";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function RootLayout({ children }) {
-  
   if (typeof window !== "undefined") {
-    var token = window.localStorage.getItem('token');
+    var token = window.localStorage.getItem("token");
   }
 
-  var [Media,setMediaList] = useState([])
-  let [subMedia, setSubMediaList] = useState([])
-  let [Category, setCategoryList] = useState([])
-  let [subCategory, setSubCategoryList] = useState([])
-  let [subKeword, setSubKeywordList] = useState([])
-  
+  var [Media, setMediaList] = useState([]);
+  let [subMedia, setSubMediaList] = useState([]);
+  let [Category, setCategoryList] = useState([]);
+  let [subCategory, setSubCategoryList] = useState([]);
+  let [subKeword, setSubKeywordList] = useState([]);
+
   useEffect(() => {
     //로컬스토리지에 저장되어 있는 토큰 받아오기
     if (typeof window !== "undefined") {
-      var token = window.localStorage.getItem('token');
+      var token = window.localStorage.getItem("token");
     }
     setMediaCategory();
     getSubscribe(token);
     mediaSubCheck();
-  }, [])
+  }, []);
 
   function setMediaCategory() {
     //전체 언론사 출력
     fetch("/api/subscribe/guest/media", {
-      method: "GET"
+      method: "GET",
     })
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
+      .then((res) => res.json())
+      .then((data) => {
         setMediaList(data.mediaList);
       });
     //전체 카테고리 출력
     fetch("/api/subscribe/guest/category", {
-      method: "GET"
+      method: "GET",
     })
-      .then(res => res.json())
-      .then(data => {
+      .then((res) => res.json())
+      .then((data) => {
         setCategoryList(data.nameList);
       });
   }
@@ -49,12 +48,11 @@ export default function RootLayout({ children }) {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        'X-AUTH-TOKEN': token,
-      }
+        "X-AUTH-TOKEN": token,
+      },
     })
-      .then(res => res.json())
-      .then(data => {
-        console.log(data.mediaList);
+      .then((res) => res.json())
+      .then((data) => {
         setSubMediaList(data.mediaList);
         setSubCategoryList(data.categoryList);
         setSubKeywordList(data.keywordList);
@@ -63,60 +61,75 @@ export default function RootLayout({ children }) {
 
   //언론사 및 카테고리 구독여부 체크
   function mediaSubCheck() {
-    console.log(Media);
-    for (i in Media){
-       if (subMedia.contains(i))
-        document.getElementById(i).checked = true;
-      else
-        document.getElementById(i).checked = false;
+    for (i in Media) {
+      if (subMedia.contains(i)) document.getElementById(i).checked = true;
+      else document.getElementById(i).checked = false;
     }
 
     for (i in subCategory)
-      if (Media.contains(i))
-        document.getElementById(i).checked = true;
-      else
-        document.getElementById(i).checked = false;
-  }
-  
-  //언론사 및 카테고리 구독하기
-  function Subscribe(e) {
-    // const nameList = [];
-    if(e.checked==false){
-      subMedia.push(e.value); 
-    }
-    else{
-      subMedia.filter((value)=>{value!==e.value});
-    }
-    console.log(subMedia);
-    fetch('/api/subscribe/media',
-    {
-      method:"POST",
-      headers:{
-        'X-AUTH-TOKEN': token,
-        "Content-Type": "application/json",
-      },
-      body:JSON.stringify({
-        'nameList':subMedia
-      })
-    })
-    .then(res => res.json())
-		.then(data => {
-      console.log(data);
-    });
+      if (Media.contains(i)) document.getElementById(i).checked = true;
+      else document.getElementById(i).checked = false;
   }
 
+  //언론사 및 카테고리 구독하기
+  function Subscribe(e) {
+    if (e.checked) {
+      setSubMediaList([...subMedia, e.value]);
+    } else {
+      setSubMediaList(subMedia.filter((item) => item !== e.value));
+
+      // subMedia.filter((value) => {
+      //   if (value === e.value) {
+      //     subMedia.pop(e.value);
+      //   }
+      // });
+      // setSubMediaList(subMedia);
+    }
+
+    fetch("/api/subscribe/media", {
+      method: "POST",
+      headers: {
+        "X-AUTH-TOKEN": token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        nameList: subMedia,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {});
+  }
+
+  function IsSubscribeMedia(value) {
+    return subMedia.includes(value);
+  }
 
   return (
     <div className="wrap">
       <div className="floatBox mb50">
         <div className="fr">
-          <Link href="/resign"><button type="button" className="btnLight mr5">내프로필</button></Link>
-          <Link href="/myinfo"><button type="button" className="btnLight mr5">구독관리</button></Link>
-          <Link href="/like"><button type="button" className="btnLight mr5">찜한기사</button></Link>
-          <Link href="/comment"><button type="button" className="btnLight mr5">댓글관리</button></Link>
+          <Link href="/resign">
+            <button type="button" className="btnLight mr5">
+              내프로필
+            </button>
+          </Link>
+          <Link href="/myinfo">
+            <button type="button" className="btnLight mr5">
+              구독관리
+            </button>
+          </Link>
+          <Link href="/like">
+            <button type="button" className="btnLight mr5">
+              찜한기사
+            </button>
+          </Link>
+          <Link href="/comment">
+            <button type="button" className="btnLight mr5">
+              댓글관리
+            </button>
+          </Link>
         </div>
       </div>
-
 
       <div className="wrap3">
         <div className="contentTitleBox">
@@ -125,9 +138,9 @@ export default function RootLayout({ children }) {
 
         <table className="tableTypeSort center">
           <colgroup>
-            <col style={{ width: '35%' }} />
-            <col style={{ width: '35%' }} />
-            <col style={{ width: '35%' }} />
+            <col style={{ width: "35%" }} />
+            <col style={{ width: "35%" }} />
+            <col style={{ width: "35%" }} />
           </colgroup>
           <thead>
             <tr>
@@ -137,20 +150,31 @@ export default function RootLayout({ children }) {
             </tr>
           </thead>
           <tbody>
-            {
-              Media.map((media) => {
-                return (
-                  <tr key={media.mediaName}>
-                    <td><img src={media.imagePath} /></td>
-                    <td>{media.mediaName}</td>
-                    <td><input type="checkbox" value={media.mediaName} id={media.mediaName} onClick={(e)=>Subscribe(e.target)} ></input></td>
-                  </tr>
-                )
-              })}
+            {Media.map((media) => {
+              return (
+                <tr key={media.mediaName}>
+                  <td>
+                    <img src={media.imagePath} />
+                  </td>
+                  <td>{media.mediaName}</td>
+                  <td>
+                    <input
+                      type="checkbox"
+                      value={media.mediaName}
+                      // id={media.mediaName}
+                      // defaultChecked={IsSubscribeMedia(media.mediaName)}
+                      checked={IsSubscribeMedia(media.mediaName)}
+                      onChange={(e) => Subscribe(e.target)}
+                      // onChange={(e) => Subscribe(e.target)}
+                      // onChange={Subscribe}
+                    ></input>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
-
 
       <div className="wrap3">
         <div className="contentTitleBox">
@@ -159,8 +183,8 @@ export default function RootLayout({ children }) {
 
         <table className="tableTypeSort center">
           <colgroup>
-            <col style={{ width: '70%' }} />
-            <col style={{ width: '30%' }} />
+            <col style={{ width: "70%" }} />
+            <col style={{ width: "30%" }} />
           </colgroup>
           <thead>
             <tr>
@@ -169,15 +193,16 @@ export default function RootLayout({ children }) {
             </tr>
           </thead>
           <tbody>
-            {
-              Category.map((category) => {
-                return (
-                  <tr key={category}>
-                    <td>{category}</td>
-                    <td><input type="checkbox" ></input></td>
-                  </tr>
-                )
-              })}
+            {Category.map((category) => {
+              return (
+                <tr key={category}>
+                  <td>{category}</td>
+                  <td>
+                    <input type="checkbox"></input>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
@@ -189,8 +214,8 @@ export default function RootLayout({ children }) {
 
         <table className="tableTypeSort center">
           <colgroup>
-            <col style={{ width: '70%' }} />
-            <col style={{ width: '30%' }} />
+            <col style={{ width: "70%" }} />
+            <col style={{ width: "30%" }} />
           </colgroup>
           <thead>
             <tr>
@@ -204,14 +229,16 @@ export default function RootLayout({ children }) {
                 <div>
                   <tr>
                     <td>{keyword}</td>
-                    <td><input type="checkbox" ></input></td>
+                    <td>
+                      <input type="checkbox"></input>
+                    </td>
                   </tr>
                 </div>
-              )
+              );
             })}
           </tbody>
         </table>
       </div>
     </div>
-  )
+  );
 }
