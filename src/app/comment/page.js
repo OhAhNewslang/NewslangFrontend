0cb999bb2de84ae16d375e12ba131de4017ecca8
gonggongtae <dnjsxo0306@gmail.com>
2023-11-03 08:58:51 +0900

    찜한기사 및 댓글관리 페이지 기능 구현

--- src/app/comment/page.js
+++ src/app/comment/page.js
@@ -1,135 +1,195 @@
-'use client'
-import Link from 'next/link';
+"use client";
+import Link from "next/link";
 // import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 // import { headers } from '../../../next.config';
 // import { data } from 'autoprefixer';
-import swal from 'sweetalert';
+import swal from "sweetalert";
 
 export default function RootLayout({ children }) {
+  //최신순 공감순 거르기
+  let [Comments, setCommentsData] = useState([]);
+  let [orderby, seturl] = useState("like");
 
+  //댓글가져오기
+  const page = 1,
+    limit = 10;
+  useEffect(() => {
+    const getCommentsData = async (page, limit) => {
+      if (typeof window !== "undefined") {
+        var token = window.localStorage.getItem("token");
+      }
+      fetch(`/api/opinions/members/${orderby}?page=${page}&limit=${limit}`, {
+        method: "GET",
+        headers: {
+          "X-AUTH-TOKEN": token,
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setCommentsData(data.opinions);
+        });
+    };
+    getCommentsData(page, limit);
+  }, [orderby]);
+
+  //의견 삭제
+  const deleteHandler = (opinionId) => {
+    //로컬스토리지에 저장되어 있는 토큰 받아오기
     if (typeof window !== "undefined") {
-        var token = window.localStorage.getItem('token');
-    }
-    //최신순 공감순 거르기
-    let [Comments, setCommentsData] = useState([])
-    let [fetchurl, seturl] = useState(["recent"])
-    
-    
-    function onClickLike() {
-        seturl("like");
-        const page = 1, limit = 10;
-        getCommentsData(page, limit);
-    }
-    function onClickLive() {
-        seturl("recent");
-        const page = 1, limit = 10;
-        getCommentsData(page, limit);
+      var token = window.localStorage.getItem("token");
     }
 
-    //댓글가져오기
-    const page = 1, limit = 10;
-    useEffect(() => {
-        getCommentsData(page, limit);
-    }, []);
+    fetch(`/api/opinions`, {
+      method: "DELETE",
+      headers: {
+        "X-AUTH-TOKEN": token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ opinionId }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setCommentsData((Comments) =>
+          Comments.filter((item) => item.opinionId !== opinionId)
+        );
+        swal({
+          text: data.resultMessage,
+          icon: "info",
+          timer: 3000,
+          buttons: false,
+        });
+      });
+  };
 
-    function getCommentsData(page, limit) {
-        fetch(`/api/opinions/members/${fetchurl}?page=${page}&limit=${limit}`, {
-            method: "GET",
-            headers: {
-                'X-AUTH-TOKEN': token
-            }
-        })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                setCommentsData(data.opinions);
-            });
+  const MakeOpinionOrderbyButton = () => {
+    var likeClassName = "btnLight mr5";
+    var recentClassName = "btnLight";
+
+    if (orderby === "like") {
+      likeClassName = "btnSelLight mr5";
+      recentClassName = "btnLight";
+    } else if (orderby === "recent") {
+      likeClassName = "btnLight mr5";
+      recentClassName = "btnSelLight";
     }
-
-    //의견삭제
-    function deleteOpinion(opinionId){
-        fetch('',{
-            method:'DELETE',
-            headers:{'X-AUTH-TOKEN': token},
-            body:{'opinionId':opinionId}}
-            .then(res=>res.json())
-            .then(data=>{
-                swal(
-                    {
-                        text:"",
-                        icon:"check",
-                        timer:3000
-                    }
-                )
-            }))
-    }
-
-
 
     return (
-        <div className="wrap">
-            <div className="floatBox mb50">
-                <div className="fr">
-                    <Link href="/resign"><button type="button" className="btnLight mr5">내프로필</button></Link>
-                    <Link href="/myinfo"><button type="button" className="btnLight mr5">구독관리</button></Link>
-                    <Link href="/like"><button type="button" className="btnLight mr5">찜한기사</button></Link>
-                    <Link href="/comment"><button type="button" className="btnLight mr5">댓글관리</button></Link>
-                </div>
-            </div>
+      <div className="left">
+        <button
+          type="button"
+          className={likeClassName}
+          onClick={() => {
+            seturl("like");
+          }}
+        >
+          공감순
+        </button>
+        <button
+          type="button"
+          className={recentClassName}
+          onClick={() => {
+            seturl("recent");
+          }}
+        >
+          최신순
+        </button>
+      </div>
+    );
+  };
 
-            <div className="contentTitleBox">
-                <h4>댓글관리</h4>
-            </div>
+  return (
+    <div className="wrap">
+      <div className="floatBox mb50">
+        <div className="fr">
+          <Link href="/resign">
+            <button type="button" className="btnLight mr5">
+              내프로필
+            </button>
+          </Link>
+          <Link href="/myinfo">
+            <button type="button" className="btnLight mr5">
+              구독관리
+            </button>
+          </Link>
+          <Link href="/like">
+            <button type="button" className="btnLight mr5">
+              찜한기사
+            </button>
+          </Link>
+          <Link href="/comment">
+            <button type="button" className="btnLight mr5">
+              댓글관리
+            </button>
+          </Link>
+        </div>
+      </div>
 
-            <div className="tableWrap4">
-                <div className="floatBox mt10 mb10">
-                    <div className="left">
-                        <button type="button" className="btnLight mr5" onClick={onClickLike}>공감순</button>
-                        <button type="button" className="btnLight" onClick={onClickLive}>최신순</button>
-                    </div>
-                </div>
+      <div className="contentTitleBox">
+        <h4>댓글관리</h4>
+      </div>
 
-                <table className="tableTypeBoard">
-                    <colgroup>
-                        <col style={{ width: '8%' }} />
-                        <col style={{ width: '61%' }} />
-                        <col style={{ width: '11%' }} />
-                        <col style={{ width: '10%' }} />
-                        <col style={{ width: '7%' }} />
-                    </colgroup>
-                    <thead>
-                        <tr>
-                            <th>No.</th>
-                            <th>제목</th>
-                            <th>공감수</th>
-                            <th>작성일</th>
-                            <th>삭제</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            Comments.map((comments, index) => {
-                                return (
-                                    <tr key={comments.opinionId}>
-                                        <td>{index + 1}</td>
-                                        <td className="tl">{comments.opinionContent}</td>
-                                        <td>{comments.likeCount}</td>
-                                        <td>{comments.opinionCreateDate}</td>
-                                        <td><input type='checkbox'></input></td>
-                                    </tr>
-                                )
-                            })
-                        }
-                    </tbody>
-                </table>
-
-                <div className="floatBox mt20 center">
-                    <button type="button" className="btnBlue wid90">수정</button>
-                </div>
-            </div>
-
+      <div className="tableWrap4">
+        <div className="floatBox mt10 mb10">
+          {MakeOpinionOrderbyButton()}
+          {/* <div className="left">
+            <button
+              type="button"
+              className="btnLight mr5"
+              onClick={() => seturl("like")}
+            >
+              공감순
+            </button>
+            <button
+              type="button"
+              className="btnLight"
+              onClick={() => seturl("like")}
+            >
+              최신순
+            </button>
+          </div> */}
         </div>
 
-    )
+        <table className="tableTypeBoard">
+          <colgroup>
+            <col style={{ width: "8%" }} />
+            <col style={{ width: "61%" }} />
+            <col style={{ width: "11%" }} />
+            <col style={{ width: "10%" }} />
+            <col style={{ width: "7%" }} />
+          </colgroup>
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>제목</th>
+              <th>공감수</th>
+              <th>작성일</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {Comments.map((comments, index) => {
+              return (
+                <tr key={comments.opinionId}>
+                  <td>{index + 1}</td>
+                  <td className="tl">{comments.opinionContent}</td>
+                  <td>{comments.likeCount}</td>
+                  <td>{comments.opinionCreateDate}</td>
+                  <td>
+                    <button
+                      className="btnUnselRed"
+                      type="button"
+                      onClick={() => deleteHandler(comments.opinionId)}
+                    >
+                      삭제
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
--- src/app/like/page.js
+++ src/app/like/page.js
@@ -1,150 +1,176 @@
-'use client'
-import Link from 'next/link';
+"use client";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import swal from 'sweetalert';
-
-
+import swal from "sweetalert";
 
 export default function RootLayout({ children }) {
-	const router = useRouter();
-	let [scrapNewsList, setScrapNewsList] = useState([])
+  const router = useRouter();
+  let [scrapNewsList, setScrapNewsList] = useState([]);
 
-	//스크랩 목록가져오기
-	const page = 1, limit = 10;
-	useEffect(() => {
-		getScrapData(page, limit);
-	}, []);
+  //스크랩 목록가져오기
+  const page = 1,
+    limit = 10;
+  useEffect(() => {
+    getScrapData(page, limit);
+  }, []);
 
-	// useEffect(() =>{
-	// },[scrapNewsList])
+  // useEffect(() =>{
+  // },[scrapNewsList])
 
-	function getScrapData(page, limit) {
-		if (typeof window !== "undefined") {
-			var token = window.localStorage.getItem('token');
-		}
-		fetch(`/api/scrap/news?page=${page}&limit=${limit}`, {
-			method: "GET",
-			headers: {
-				"Content-Type": "application/json",
-				'X-AUTH-TOKEN': token,
-			}
-		})
-			.then(res => res.json())
-			.then(data => {
-				const code = data.result.resultCode;
-				const msg = data.result.resultMessage;
-				switch (code) {
-					case '200':
-						// console.log(data.scrapNewsList);
-						setScrapNewsList(data.scrapNewsList);
-						//화면 새로고침
-						router.refresh();
-					case '404'://스크랩 뉴스없음
-						swal({
-							text: msg,
-							icon: "info",
-							timer:3000,
-							button:false,
-						  });
-						router.refresh();
-						break;
-					default://로그인정보없음
-						router.refresh();
-						swal({
-							text: msg,
-							icon: "warning",
-							timer:3000,
-							button:false,
-						  });
-						break;
-				}
-			}
-			);
-	}
+  function getScrapData(page, limit) {
+    if (typeof window !== "undefined") {
+      var token = window.localStorage.getItem("token");
+    }
+    fetch(`/api/scrap/news?page=${page}&limit=${limit}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "X-AUTH-TOKEN": token,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const code = data.result.resultCode;
+        const msg = data.result.resultMessage;
+        switch (code) {
+          case "200":
+            // console.log(data.scrapNewsList);
+            setScrapNewsList(data.scrapNewsList);
+            //화면 새로고침
+            router.refresh();
+            break;
+          case "404": //스크랩 뉴스없음
+            swal({
+              text: msg,
+              icon: "info",
+              timer: 3000,
+              button: false,
+            });
+            router.refresh();
+            break;
+          default: //로그인정보없음
+            router.refresh();
+            swal({
+              text: msg,
+              icon: "warning",
+              timer: 3000,
+              button: false,
+            });
+            break;
+        }
+      });
+  }
 
-	const makeCancelScrapButton = (newsUrl) => {
-		return(
-			<button class="btnUnselRed" type="button" onClick={() => 
-				deleteHandler(newsUrl)
-			}>취소</button>
-		);
-	}
+  const makeCancelScrapButton = (newsUrl) => {
+    return (
+      <button
+        className="btnUnselRed"
+        type="button"
+        onClick={() => deleteHandler(newsUrl)}
+      >
+        취소
+      </button>
+    );
+  };
 
-	//댓글 삭제
-	const deleteHandler = (newsUrl) => {
-		//로컬스토리지에 저장되어 있는 토큰 받아오기
-		if (typeof window !== "undefined") {
-			var token = window.localStorage.getItem('token');
-		}
+  //댓글 삭제
+  const deleteHandler = (newsUrl) => {
+    //로컬스토리지에 저장되어 있는 토큰 받아오기
+    if (typeof window !== "undefined") {
+      var token = window.localStorage.getItem("token");
+    }
 
-		fetch(`/api/scrap/news`, {
-			method: "DELETE",
-			headers: {
-				'X-AUTH-TOKEN': token,
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ newsUrl }),
-		})
-			.then(res => res.json())
-			.then(data => {
-				// alert(data.result.resultMessage + "\r\n" + newsUrl);
-				setScrapNewsList(scrapNewsList => scrapNewsList.filter((item) => item.newsUrl !== newsUrl));
-			});
-	}
+    fetch(`/api/scrap/news`, {
+      method: "DELETE",
+      headers: {
+        "X-AUTH-TOKEN": token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ newsUrl }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        // alert(data.result.resultMessage + "\r\n" + newsUrl);
+        setScrapNewsList((scrapNewsList) =>
+          scrapNewsList.filter((item) => item.newsUrl !== newsUrl)
+        );
+      });
+  };
 
-	return (
+  return (
+    <div className="wrap">
+      <div className="floatBox mb50">
+        <div className="fr">
+          <Link href="/resign">
+            <button type="button" className="btnLight mr5">
+              내프로필
+            </button>
+          </Link>
+          <Link href="/myinfo">
+            <button type="button" className="btnLight mr5">
+              구독관리
+            </button>
+          </Link>
+          <Link href="/like">
+            <button type="button" className="btnLight mr5">
+              찜한기사
+            </button>
+          </Link>
+          <Link href="/comment">
+            <button type="button" className="btnLight mr5">
+              댓글관리
+            </button>
+          </Link>
+        </div>
+      </div>
 
-		<div className="wrap">
-			<div className="floatBox mb50">
-				<div className="fr">
-					<Link href="/resign"><button type="button" className="btnLight mr5">내프로필</button></Link>
-					<Link href="/myinfo"><button type="button" className="btnLight mr5">구독관리</button></Link>
-					<Link href="/like"><button type="button" className="btnLight mr5">찜한기사</button></Link>
-					<Link href="/comment"><button type="button" className="btnLight mr5">댓글관리</button></Link>
-				</div>
-			</div>
-
-			<div className="tableWrap4">
-				<table className="tableTypeBoard">
-					<colgroup>
-						<col style={{ width: '8%' }} />
-						<col style={{ width: '45%' }} />
-						<col style={{ width: '12%' }} />
-						<col style={{ width: '12%' }} />
-						<col style={{ width: '16%' }} />
-						<col style={{ width: '7%' }} />
-					</colgroup>
-					<thead>
-						<tr>
-							<th>No.</th>
-							<th>제목</th>
-							<th>언론사</th>
-							<th>작성자</th>
-							<th>작성일</th>
-							<th>스크랩</th>
-						</tr>
-					</thead>
-					<tbody>
-						{scrapNewsList.map((news, index) => {
-							return (
-									<tr key={news.url}>
-										<td>{index + 1}</td>
-										<td className="tl"><a href={news.newsUrl}>{news.title}</a></td>
-										<td>{news.mediaName}</td>
-										<td>{news.reporter}</td>
-										<td>{news.postDateTime}</td>
-										<td>{makeCancelScrapButton(news.newsUrl)}</td>
-									</tr>
-							)
-						})}
-					</tbody>
-				</table>
-				<div className="floatBox mt10 center">
-					<button type="button" className="btnBlue wid90">수정</button>
-				</div>
-			</div>
-
-		</div>
-	)
+      <div className="tableWrap4">
+        <table className="tableTypeBoard">
+          <colgroup>
+            <col style={{ width: "8%" }} />
+            <col style={{ width: "45%" }} />
+            <col style={{ width: "12%" }} />
+            <col style={{ width: "12%" }} />
+            <col style={{ width: "16%" }} />
+            <col style={{ width: "7%" }} />
+          </colgroup>
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>제목</th>
+              <th>언론사</th>
+              <th>작성자</th>
+              <th>작성일</th>
+              <th>스크랩</th>
+            </tr>
+          </thead>
+          <tbody>
+            {scrapNewsList.map((news, index) => {
+              return (
+                <tr key={news.newsUrl}>
+                  <td>{index + 1}</td>
+                  <td className="tl">
+                    <a
+                      href={`/view?newsUrl=${news.newsUrl}`}
+                      onClick={() => {
+                        localStorage.setItem("newsUrl", news.newsUrl);
+                      }}
+                    >
+                      {news.title}
+                    </a>
+                    {/* <a href={news.newsUrl}>{news.title}</a> */}
+                  </td>
+                  <td>{news.mediaName}</td>
+                  <td>{news.reporter}</td>
+                  <td>{news.postDateTime}</td>
+                  <td>{makeCancelScrapButton(news.newsUrl)}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
